6ae146151a97413d0d14da2299e8d900ad449003
Boubacar S. BARRY <sidbouba@gmail.com>
2016-08-13 15:05:00 +0200

    Return emepty string when credit card number param is not  vali
    i.e when is   empty  or  null or undefined

--- src/components/validation-credit-card.js
+++ src/components/validation-credit-card.js
@@ -1,14 +1,17 @@
-
 /**
  * Validate the credit card number based on the following validation rules
  * Visa cards start with 4.  Visa cards are valid if they have 13 to 16 digits.
  * Mastercard cards start with 5. Mastercard cards are valid if it has 16 digits.
  * American Express cards start with 3. The 2nd digit is either 4 or 7.
  *  American Express cards are valid if it has 15 digits.
- * 
  * @param {string} creditCardNumber
  * @returns {string}
  */
 export function validate(creditCardNumber = '') {
-
+    if (creditCardNumber === '' ||
+        creditCardNumber === undefined ||
+        creditCardNumber === null) {
+        return '';
+    }
+    return '';
 }
--- test/unit/specs/validation-credit-card.spec.js
+++ test/unit/specs/validation-credit-card.spec.js
@@ -1,7 +1,9 @@
 import { validate } from '../../../src/components/validation-credit-card';
 
 describe(' validate', () => {
-  it('should return empty string when creditCardNumber param is empty', () => {
+  it('should return empty string when creditCardNumber param is empty or undefined or null', () => {
     expect(validate()).to.equal('');
+    expect(validate(undefined)).to.equal('');
+    expect(validate(null)).to.equal('');
   });
-})
+});
